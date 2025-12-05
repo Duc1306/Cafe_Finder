0de536ab406acd9d3312c0405f678c96b1af3415
huy_ln <huy_ln@ehicas.com.vn>
2025-12-05 21:06:13 +0700

    Feat:User profile

--- frontend/src/router/userRoutes.jsx
+++ frontend/src/router/userRoutes.jsx
@@ -1,12 +1,13 @@
 import { Route } from "react-router-dom";
 import { UserLayout } from "../components/layouts/user-layout";
 import UserDashboardPage from "../pages/user/userDashboard";
+import UserProfile from "../pages/user/userProfile";
 
 export function UserRoutes() {
   return (
     <Route element={<UserLayout />}>
       <Route path="/user/dashboard" element={<UserDashboardPage />} />
-      
+      <Route path="/user/profile" element={<UserProfile />} />
     </Route>
   );
 }
--- frontend/src/services/UserDashBoardService.js
+++ frontend/src/services/UserDashBoardService.js
@@ -1,6 +1,19 @@
 import api from './api';
 
+// Dashboard (giữ nguyên)
 export const getUserDashboard = async () => {
   const res = await api.get('/users/dashboard');
+  return res.data.data;
+};
+
+// Lấy profile user
+export const getUserProfile = async () => {
+  const res = await api.get('/users/profile');
+  return res.data.data;
+};
+
+// Cập nhật profile user
+export const updateUserProfile = async (data) => {
+  const res = await api.put('/users/profile', data);
   return res.data.data;
 };
