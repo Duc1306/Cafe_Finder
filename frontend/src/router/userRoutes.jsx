09984e5f9a67bc0d4f19721861008f9980a57f2c
PlatypusInAHat <binh.nc225695@sis.hust.edu.vn>
2025-12-08 17:40:31 +0700

    sửa

--- frontend/src/router/userRoutes.jsx
+++ frontend/src/router/userRoutes.jsx
@@ -3,9 +3,8 @@ import { UserLayout } from "../components/layouts/user-layout";
 import UserDashboardPage from "../pages/user/userDashboard";
 import UserProfile from "../pages/user/userProfile";
 import CafeSearch from "../pages/user/CafeSearch";
-import Favorites from "../pages/user/Favorites";
 import CafeDetail from "../pages/user/CafeDetail";
-import UserReviews from "../pages/user/userReviews";
+
 
 export function UserRoutes() {
   return (
@@ -14,8 +13,6 @@ export function UserRoutes() {
       <Route path="/user/profile" element={<UserProfile />} />
       <Route path="/user/cafes/search" element={<CafeSearch />} />
       <Route path="/user/cafes/:id" element={<CafeDetail />} />
-      <Route path="/user/favorites" element={<Favorites />} />
-      <Route path="/user/review" element={<UserReviews />} />
     </Route>
   );
 }
