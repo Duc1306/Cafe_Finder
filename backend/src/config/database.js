6b8af342fe839a48973398e9c97c1d0b4f7dcacc
Duc1306 <trinhduc1362k3@gmail.com>
2025-12-15 01:12:05 +0700

    fix: use internal database url on rende

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,19 +3,6 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-    keepAlive: true,
-  },
 });
 
 const testConnection = async () => {
