de0a27d48b5c139b9a85c1edcc80f24ea8653b88
huy_ln <huy_ln@ehicas.com.vn>
2025-12-25 21:13:53 +0700

    update

--- frontend/src/pages/user/userProfile.jsx
+++ frontend/src/pages/user/userProfile.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import {
   getUserProfile,
   updateUserProfile,
-} from "../../services/userDashboardService";
+} from "../../services/userDashBoardService";
 import { Input, Button, message, Spin, Avatar } from "antd";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeftOutlined, UploadOutlined } from "@ant-design/icons";
-import { uploadUserAvatar } from "../../services/userDashboardService";
+import { uploadUserAvatar } from "../../services/userDashBoardService";
 export default function UserProfile() {
   const [profile, setProfile] = useState(null);
   const [fullName, setFullName] = useState("");
